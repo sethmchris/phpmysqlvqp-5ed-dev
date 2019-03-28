89479bed0a8f9e8d101884574adffbb6b3c4e229
sethmchris <sethmchris@gmail.com>
2019-03-27 19:58:07 -0600

    Change all instances of  to

--- ch02/script_02_04/handle_form.php
+++ ch02/script_02_04/handle_form.php
@@ -13,42 +13,44 @@
 <body>
 <?php # Script 2.4 - handle_form.php #3
 
+// Rewrite handle_form.php (Script 2.4) to use $_POST instead of $_REQUEST.
+
 // Validate the name:
-if (!empty($_REQUEST['name'])) {
-	$name = $_REQUEST['name'];
+if (!empty($_POST['name'])) {
+	$name = $_POST['name'];
 } else {
 	$name = NULL;
 	echo '<p class="error">You forgot to enter your name!</p>';
 }
 
 // Validate the email:
-if (!empty($_REQUEST['email'])) {
-	$email = $_REQUEST['email'];
+if (!empty($_POST['email'])) {
+	$email = $_POST['email'];
 } else {
 	$email = NULL;
 	echo '<p class="error">You forgot to enter your email address!</p>';
 }
 
 // Validate the comments:
-if (!empty($_REQUEST['comments'])) {
-	$comments = $_REQUEST['comments'];
+if (!empty($_POST['comments'])) {
+	$comments = $_POST['comments'];
 } else {
 	$comments = NULL;
 	echo '<p class="error">You forgot to enter your comments!</p>';
 }
 
-// 3a. Rewrite the gender conditional in handle_form.php (Script 2.4) as one conditional instead of two nested ones. Hint: You’ll need to use the AND operator.
 
+// 3. Rewrite the gender conditional in handle_form.php (Script 2.4) as one conditional instead of two nested ones. Hint: You’ll need to use the AND operator.
 // Validate the gender:
-$gender = $_REQUEST['gender'];
+$gender = $_POST['gender'];
 
-if (isset($_REQUEST['gender']) && $gender == 'M') { // Gender is set and value is equal to M
+if (isset($_POST['gender']) && $gender == 'M') { // Gender is set and value is equal to M
 	$greeting = '<p><strong>Good day, Sir!</strong></p>';
-} elseif (isset($_REQUEST['gender']) && $gender == 'F') { // Gender is set and value is equal to F
+} elseif (isset($_POST['gender']) && $gender == 'F') { // Gender is set and value is equal to F
 	$greeting = '<p><strong>Good day, Madam!</strong></p>';
-} elseif (isset($_REQUEST['gender']) && $gender = NULL ){ // Unacceptable value.
+} elseif (isset($_POST['gender']) && $gender = NULL ){ // Unacceptable value.
 	echo '<p class="error">Gender should be either "M" or "F"!</p>';
-} else { // $_REQUEST['gender'] is not set.
+} else { // $_POST['gender'] is not set.
 	$gender = NULL;
 	echo '<p class="error">You forgot to select your gender!</p>';
 }
