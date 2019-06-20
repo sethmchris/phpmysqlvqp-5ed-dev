01d2e88e8d3eadd36a890e40279678b20f86db4f
sethmchris <sethmchris@gmail.com>
2019-06-20 15:40:24 -0600

    Added functionality for changing user's password

--- ch10/sitename/edit_user.php
+++ ch10/sitename/edit_user.php
@@ -65,24 +65,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$e = mysqli_real_escape_string($dbc, trim($_POST['email']));
 	}
 
-	// Check for the current password:
-	if (empty($_POST['pass'])) {
-		$errors[] = 'You forgot to enter your current password.';
-	} else {
-		$p = mysqli_real_escape_string($dbc, trim($_POST['pass']));
-	}
-
+	# 2a. Modify edit_user.php so that you can also change a user's password
 	// Check for a new password and match
 	// against the confirmed password:
-	if (!empty($_POST['pass1'])) {
+	if (!empty($_POST['pass1']) && (!empty($_POST['pass2']))) {
 		if ($_POST['pass1'] != $_POST['pass2']) {
 			$errors[] = 'Your new password did not match the confirmed password.';
 		} else {
 			$np = mysqli_real_escape_string($dbc, trim($_POST['pass1']));
 		}
-	} else {
-		$errors[] = 'You forgot to enter your new password.';
-	}
+	} // 2b. If pass1 and pass2 are empty, then do nothing
 
 	if (empty($errors)) { // If everything's OK.
 
@@ -92,7 +84,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		if (mysqli_num_rows($r) == 0) {
 
 			// Make the query:
-			$q = "UPDATE users SET first_name='$fn', last_name='$ln', email='$e' WHERE user_id=$id LIMIT 1";
+			# 2c. If password values are submitted, update the password in the database as well
+			if (!empty($_POST['pass1']) && (!empty($_POST['pass2']))) {
+				$q = "UPDATE users SET first_name='$fn', last_name='$ln', email='$e', pass=SHA1('$np') WHERE user_id=$id LIMIT 1";
+			} else { // 2c. If these inputs are left blank, do not update the update the password in the database
+				$q = "UPDATE users SET first_name='$fn', last_name='$ln', email='$e' WHERE user_id=$id LIMIT 1";
+			}
 			$r = @mysqli_query($dbc, $q);
 			if (mysqli_affected_rows($dbc) == 1) { // If it ran OK.
 
@@ -132,10 +129,14 @@ if (mysqli_num_rows($r) == 1) { // Valid user ID, show the form.
 	$row = mysqli_fetch_array($r, MYSQLI_NUM);
 
 	// Create the form:
+	# 2d. Add password inputs to the form
 	echo '<form action="edit_user.php" method="post">
 <p>First Name: <input type="text" name="first_name" size="15" maxlength="15" value="' . $row[0] . '"></p>
 <p>Last Name: <input type="text" name="last_name" size="15" maxlength="30" value="' . $row[1] . '"></p>
 <p>Email Address: <input type="email" name="email" size="20" maxlength="60" value="' . $row[2] . '"> </p>
+<p>New Password: <input type="password" name="pass1" size="10" maxlength="20" value="' . $row[3] . 
+'" ></p>
+<p>Confirm New Password: <input type="password" name="pass2" size="10" maxlength="20" value="' . $row[4] . '" ></p>
 <p><input type="submit" name="submit" value="Submit"></p>
 <input type="hidden" name="id" value="' . $id . '">
 </form>';
