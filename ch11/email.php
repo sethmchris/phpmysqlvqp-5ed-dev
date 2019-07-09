21a3956bf7e51f07b0d954f1bab68d3cec7111b2
sethmchris <sethmchris@gmail.com>
2019-07-09 13:32:25 -0600

    Replaced with custom HTML form

--- ch11/email.php
+++ ch11/email.php
@@ -2,11 +2,11 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Contact Me</title>
+	<title>Student Progress Calculator</title>
 </head>
 <body>
-<h1>Contact Me</h1>
-<?php # Script 11.1 - email.php
+<h1>Student Progress Calculator</h1>
+<?php # Script 11.1 - modified version of email.php
 
 // Check for form submission:
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -37,12 +37,35 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 // Create the HTML form:
 ?>
-<p>Please fill out this form to contact me.</p>
+<!-- <p>Please fill out this form to contact me.</p>
 <form action="email.php" method="post">
 	<p>Name: <input type="text" name="name" size="30" maxlength="60" value="<?php if (isset($_POST['name'])) echo $_POST['name']; ?>"></p>
 	<p>Email Address: <input type="email" name="email" size="30" maxlength="80" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>"></p>
 	<p>Comments: <textarea name="comments" rows="5" cols="30"><?php if (isset($_POST['comments'])) echo $_POST['comments']; ?></textarea></p>
 	<p><input type="submit" name="submit" value="Send!"></p>
+</form> -->
+<form action="email.php" method="post">
+
+	<fieldset><legend>Enter your information in the form below:</legend>
+
+	<p><label>Student Name: <input type="text" name="student-name" size="20" maxlength="40"></label></p>
+	<p><label>Instructor Name: 
+		<select name="instructor-name">
+			<option value="">--</option>
+			<option value="john">John</option>
+			<option value="juan">Juan</option>
+			<option value="don">Don</option>
+			<option value="james">James</option>
+		</select>
+		</label></p>
+	<p><label>Date: <input type="date" name="date"></label></p>
+
+	<p><label>Comments: <textarea name="comments" rows="3" cols="40"></textarea></label></p>
+
+	</fieldset>
+
+	<p align="center"><input type="submit" name="submit" value="Submit My Information"></p>
+
 </form>
 </body>
 </html>
