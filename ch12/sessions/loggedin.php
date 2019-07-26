c215e400b271376dfd1b9ac8c7fa8ea19f5c8e38
sethmchris <sethmchris@gmail.com>
2019-07-26 10:53:16 -0600

    Updated to latest version

--- ch12/sessions/loggedin.php
+++ ch12/sessions/loggedin.php
@@ -1,20 +1,22 @@
-<?php # Modified version of Script 12.4 - loggedin.php
+<?php # Modified version of Script 12.13 - loggedin.php #3
 // The user is redirected here from login.php
 
 session_start();
 
 // If no session is present, redirect the user:
-if (!isset($_SESSION['user_id'])) {
+// Also vvalidate the HTTP_USER_AGENT
+if (!isset($_SESSION['agent']) OR ($_SESSION['agent'] != md5($_SERVER['HTTP_USER_AGENT']) )) {
 
+  // Need the functions:
   require('includes/login_functions.inc.php');
   redirect_user();
 
 }
-  // Include the page header
+  // Set the page title and include the HTML header
   $page_title = 'Logged In!';
   include('includes/header.html');
 
-  // Welcome the user, referencing the cookie
+  // Print a customized message:
   echo "<h1>Logged In!</h1>
   <p>You are now logged in, {$_SESSION['first_name']}!</p>
   <p><a href=\"logout.php\">Logout</a></p>";
