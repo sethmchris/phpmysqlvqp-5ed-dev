85ba7efc121ee1671a226c002258cc8dedd4f8bc
sethmchris <sethmchris@gmail.com>
2019-07-26 10:50:31 -0600

    Added comments, updated scripts for security

--- ch12/sessions/login.php
+++ ch12/sessions/login.php
@@ -1,28 +1,41 @@
-<?php # Modified version of Script 12.3 - login.php
+<?php # Modified version of Script 12.12 - login.php #4
 // This page processes the login form submission.
-// Upon successful login, the user is redirected.
-// Two included files are necessary.
-// Send NOTHING to the web browser prior to the setcookie() lines!
+// The script now stores the HTTP_USER_AGENT value for added security
 
+// Check if the form has been submitted:
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+  // Need two helper files:
   require('includes/login_functions.inc.php');
   require('../mysqli_connect.php');
 
-  // Validate the form data
+  // Check the login
   list($check, $data) = check_login($dbc, $_POST['email'], $_POST['pass']);
 
-  // If the user entered the correct information, log them in:
   if ($check) { // OK!
+
+    // Set the session data:
     session_start();
     $_SESSION['user_id'] = $data['user_id'];
     $_SESSION['first_name'] = $data['first_name'];
+
+    // Store the HTTP_USER_AGENT:
+    $_SESSION['agent'] = sha1($_SERVER['HTTP_USER_AGENT']);
   
-    // Redirect the user to another page 
+    // Redirect:
     redirect_user('loggedin.php');
-  } else {
+
+  } else { // Unsuccessful!
+
+    // Assign $data to $errors for login_page.inc.php
     $errors = $data;
-  } // end of $check IF
-  mysqli_close($dbc);
-}
+  
+  }
+  
+  mysqli_close($dbc); // Close the database connection
+
+} // End of the main submit conditional
+
+// Create the page:
 include('includes/login_page.inc.php');
 ?>
--- ch12/sessions/logout.php
+++ ch12/sessions/logout.php
@@ -1,4 +1,4 @@
-<?php # Modified version of Script 12.6 - logout.php
+<?php # Modified version of Script 12.11 - logout.php #2
 // This page lets the user logout.
 
 session_start(); // Access the existing session.
@@ -6,18 +6,22 @@ session_start(); // Access the existing session.
 // If no cookie is present, redirect the user:
 if (!isset($_SESSION['user_id'])) {
 
+  // Need the functions:
   require('includes/login_functions.inc.php');
   redirect_user();
 
-} else { // Delete the session
-  $_SESSION = [];
+} else { // Cancel the session
+  $_SESSION = []; // Clear the variables
   session_destroy();
-  setcookie('first_name', '', time()-3600, '/', '', 0, 0);
+  session_destroy(); // Destroy the session itself
+  setcookie('PHPSESSID', '', time()-3600, '/', '', 0, 0); // Destroy the cookie
 }
 
+// Set the page title and include the HTML header:
 $page_title = 'Logged Out!';
 include('includes/header.html');
 
+// Print a customized message
 echo "<h1>Logged Out!</h1>
 <p>You are now logged out!</p>";
 
