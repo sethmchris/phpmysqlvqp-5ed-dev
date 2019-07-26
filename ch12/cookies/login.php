9169d422a4cdef2307ec0e48148abfd41bd16e2d
sethmchris <sethmchris@gmail.com>
2019-07-26 10:28:12 -0600

    Added parameters to initial setcookie() and changed name of darkmode to dark_mode in html

--- ch12/cookies/login.php
+++ ch12/cookies/login.php
@@ -13,10 +13,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
   // If the user entered the correct information, log them in:
   if ($check) { // OK!
-    setcookie('user_id', $data['user_id']);
-    setcookie('first_name', $data['first_name']);
+    setcookie('user_id', $data['user_id'], time()+3600, '/', '', 0, 0);
+    setcookie('first_name', $data['first_name'], time()+3600, '/', '', 0, 0);
     # 5b. Set the cookie to the $_POST value in login.php
-    setcookie('dark_mode', $_POST['dark_mode'] );
+    setcookie('dark_mode', $_POST['dark_mode'], time()+3600, '/', '', 0, 0);
   
     // Redirect the user to another page 
     redirect_user('loggedin.php');
